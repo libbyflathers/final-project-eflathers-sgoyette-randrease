5c9a3459a85e16406a6ee498891bdab4dad5f649
Libby Flathers <libbyflathers@gmail.com>
2025-05-10 16:43:18 -0400

    Troubleshooting 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
